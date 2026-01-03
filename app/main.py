456751e8826bab7522542ccf7dde3063de9318a7
Arielson Souza <arielsonalt@gmail.com>
2026-01-02 21:39:24 -0300

    Simple chat developd

--- app/main.py
+++ app/main.py
@@ -1,27 +1,59 @@
 import streamlit as st
-from openai import OpenAI
 from langchain_openai import ChatOpenAI
-import os
+from dotenv import load_dotenv
 
-openai_key = os.environ.get("OPENAI_API_KEY")
+load_dotenv()
+llm = ChatOpenAI(model="gpt-5", temperature=0, stream_usage=True)
 
-print(openai_key)
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+# messages = [
+#     (
+#         "system",
+#         "You are travel assistant. Help the user informing about travel questions."
+#     ),
+#     ("human",
+#      "I don't know how to organize my bag. Can you haelp me?")
+# ]
 
-llm = ChatOpenAI(
-    model="gpt-5",
-    temperature=0,
-    stream_usage=True
-)
+st.set_page_config(page_title ="Chat", layout="centered")
+st.title("")
 
 
-messages = [
-    (
-        "system",
-        "You are travel assistant. Help the user informing about travel questions."
-    ),
-    ("human",
-     "I don't know how organize my bag. Can you haelp me?")
-]
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-print(llm.invoke(messages))
+messages = st.session_state["messages"]
+
+for type, content in messages:
+    chat = st.chat_message(type)
+    chat.markdown(content)
+
+
+print(messages)
+
+in_message = st.chat_input("Send your question:")
+
+if in_message:
+    messages.append(("human", in_message))
+    chat = st.chat_message("human")
+    chat.markdown(in_message)
+
+    response = llm.invoke(in_message)
+    
+    messages.append(("ai", response.content))
+    chat = st.chat_message("ai")
+    chat.markdown(response.content)
+
+
+# if in_message:
+#     chat = st.chat_message("human")
+#     chat.markdown(in_message)
+#     response = llm.invoke(in_message)
+#     chat = st.chat_message("ai")
+#     chat.markdown(response.content)
+
+
+
+
+
+
+# print(llm.invoke(messages).content)
